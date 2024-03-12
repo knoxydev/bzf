9e3980f82c71a4bed84fd9156669ddce0a9bdb1c
knoxydev <nkgithubnarkuziev108@gmail.com>
2024-03-13 03:57:16 +0500

    update: minor changes & new files

---
+++ src/get.rs
@@ -0,0 +1,4 @@
+pub mod get_md
+{
+	
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
+#![allow(warnings)]
+
+
+// MODULES
+mod init;
+pub use crate::init::init_md;
+
+mod keyboard;
+pub use crate::keyboard::keyboard_md;
+
+mod print;
+
+
+use std::env;
+use std::thread;
+use std::time::Duration;
+use std::io::{self, Write};
+
+
 fn main()
 {
-	println!("Hello, world!");
+	keyboard_md::start();
 }
