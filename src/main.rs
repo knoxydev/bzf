50f5abde74dcb87fc1802da372024fc6ee70e7ec
knoxydev <nkgithubnarkuziev108@gmail.com>
2024-03-21 11:50:59 +0500

    add: first list render

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ pub use crate::keyboard::keyboard_md;
 mod print;
 
 
+// PACKAGES
 use std::env;
 use std::thread;
 use std::time::Duration;
@@ -19,5 +20,6 @@ use std::io::{self, Write};
 
 fn main()
 {
+	print::print_md::start(0 as i32);
 	keyboard_md::start();
 }
