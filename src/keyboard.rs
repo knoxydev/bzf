96f60be2b911e40ed6356e473f8bd695c96b9503
knoxydev <nkgithubnarkuziev108@gmail.com>
2024-03-24 03:59:30 +0500

    add: keyevent > q | update: minor changes

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -1,14 +1,17 @@
 pub mod keyboard_md
 {
+  use crate::state::state_md::Core;
   use crate::print::print_md;
+  use crate::state::state_md;
   use device_query::{DeviceQuery, DeviceState, Keycode};
 
 
   pub fn start()
   {
+    let core = state_md::start();
   	let device_state = DeviceState::new();
-  	let mut prev_keys = vec![];
-  	let mut x: i32 = 0;
+  	let mut prev_keys = Vec::new();    
+  	let mut x: i64 = 0;
 	
 
   	loop
@@ -18,23 +21,30 @@ pub mod keyboard_md
       {
         if keys.contains(&Keycode::Up)
         {
-        	// IF x == 0 -> MOVE INDICATOR TO LAST ELEMENT OF ARR
-        	// EL -> MOVE INDICATOR UPPER
-        	x = if (x == 0) { (print_md::get_folder_length() as i32 - 1) }
-        	else { x - 1 }
+          // IF x == 0 -> MOVE INDICATOR TO LAST ELEMENT OF ARR
+          // EL -> MOVE INDICATOR UPPER
+          x = if (x == 0) { (core.len() - 1) }
+          else { x - 1 }
         }
         if keys.contains(&Keycode::Down)
         {
-        	// IF x == 0 -> MOVE INDICATOR TO FIRST ELEMENT OF ARR
-        	// EL -> MOVE INDICATOR LOWER
-        	x = if (x == (print_md::get_folder_length() as i32) - 1) { 0 }
-        	else { x + 1 }
+          // IF x == 0 -> MOVE INDICATOR TO FIRST ELEMENT OF ARR
+          // EL -> MOVE INDICATOR LOWER
+          x = if (x == core.len() - 1) { 0 }
+          else { x + 1 }
         }
-        if keys.contains(&Keycode::Right) { }
+        if keys.contains(&Keycode::Right)
+        {
+          //println!("{:?}", x);
+
+          state_md::start();
+          return;
+        }
         if keys.contains(&Keycode::Q)
         {
-        	println!("\n[EXIT]\n");
-        	return;
+          core.start();
+          println!("\n[EXIT]\n");
+          return;
         }
 
         print_md::start(x);
