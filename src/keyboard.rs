3e304ce57e027d872c787b47da930f9f5dc70015
knoxydev <nkgithubnarkuziev108@gmail.com>
2024-03-26 03:59:44 +0500

    add: keycode > left

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -1,5 +1,6 @@
 pub mod keyboard_md
 {
+  use std::path::PathBuf;
   use crate::state::state_md::Core;
   use crate::print::print_md;
   use crate::state::state_md;
@@ -8,11 +9,13 @@ pub mod keyboard_md
 
   pub fn start()
   {
-    let core = state_md::start();
   	let device_state = DeviceState::new();
+    let mut core = state_md::start();
   	let mut prev_keys = Vec::new();    
   	let mut x: i64 = 0;
-	
+    
+    print_md::start(&core, 0 as i64);    
+
 
   	loop
   	{
@@ -23,31 +26,43 @@ pub mod keyboard_md
         {
           // IF x == 0 -> MOVE INDICATOR TO LAST ELEMENT OF ARR
           // EL -> MOVE INDICATOR UPPER
-          x = if (x == 0) { (core.len() - 1) }
+          x = if (x == 0) { (state_md::len(&core.data) - 1) }
           else { x - 1 }
         }
         if keys.contains(&Keycode::Down)
         {
           // IF x == 0 -> MOVE INDICATOR TO FIRST ELEMENT OF ARR
           // EL -> MOVE INDICATOR LOWER
-          x = if (x == core.len() - 1) { 0 }
+          x = if (x == state_md::len(&core.data) - 1) { 0 }
           else { x + 1 }
         }
         if keys.contains(&Keycode::Right)
         {
-          //println!("{:?}", x);
+          let next_id: &String = &core.data[x as usize];
+          let mut next_path = &mut core.curr_path;
+          next_path.push(PathBuf::from(next_id));
 
-          state_md::start();
-          return;
+          core.data = state_md::get(next_path.to_path_buf());
+          core.curr_path = next_path.to_path_buf();
+
+          x = 0;                
+        }
+        if keys.contains(&Keycode::Left)
+        {
+          let mut prev = core.curr_path.clone();
+          prev.pop();
+
+          core.curr_path = prev.to_path_buf();
+          core.data = state_md::get(prev.to_path_buf());
+          
+          x = 0;
         }
         if keys.contains(&Keycode::Q)
-        {
-          core.start();
-          println!("\n[EXIT]\n");
+        {  
+          println!("{}", print_md::keycode_q("\n[EXIT]\n".to_string()));
           return;
         }
-
-        print_md::start(x);
+        print_md::start(&core, x);
       }
       prev_keys = keys;
     }
